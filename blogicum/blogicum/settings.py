4181dd002a31ce70a50c7b3b8952b9446a8bac14
Стас Басов <semenovpolk2018@yandex.ru>
2025-03-21 12:02:05 +0300

    add templates html

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -118,7 +118,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
+STATICFILES_DIRS = [
+    BASE_DIR / 'static_file'
+]
 STATIC_URL = 'static/'
 
 # Default primary key field type
